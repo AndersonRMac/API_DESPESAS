ca573df1ff16863bb3162f24e7a53e8439b14075
Your Name <anderson.ramosmaciel@gmail.com>
2025-07-16 09:32:18 -0300

    Ajuste da conexão com o banco MySQL

--- src/main/java/com/finance/despesas/model/Lancamento.java
+++ src/main/java/com/finance/despesas/model/Lancamento.java
@@ -1,5 +1,6 @@
 package com.finance.despesas.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,21 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_lancamento")
 public class Lancamento {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private int tipo;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String descricao;
+
+    @Column(nullable = false)
     private double valor;
+
 }
